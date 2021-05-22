22b28684a1d6e97bc72af923b4c1a0d720824f98
Kris Tina <kristyguerrero20@gmail.com>
2021-05-22 16:42:32 -0500

    Updated the ReadME file and added some changes to the script.js

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # password-generator-project
+
+Created the variables for uppercase letters, lowercase letters, special characters, and numbers 
+
+Created the bigArray using the stringo concat method
+
+Created the alert boxes to ensure password lenght is between 8-128 characters
+
+Created the confirm boxes on upper, lower, special characters and numbers boxes
+
--- script.js
+++ script.js
@@ -1,37 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var upperCaseletters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N","O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var LowerCaseLetters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z',
-];
+var LowerCaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var specialCharletters =  [ '^', '?', ':',  ',', ')','(','}','{', ']','[','~', '-', '_', '.'];
 var numbersChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 // Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -84,6 +58,9 @@ function generatePassword() {
     bigArray=bigArray.concat(numbersChar);
   }
   console.log(bigArray)
+
+
+  return password
 }
   // var isUpperCase = confirm("Do you want it to contain uppercase letters?")
 
